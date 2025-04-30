28480a5fcf9964098f76596024fd8eee27dd2472
14thHerrscher <duddndcnfrur@naver.com>
2025-04-30 09:01:58 +0100

    9thweek

---
+++ pwm1.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+LED = 10
+
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+	GPIO.output(LED, GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(LED, GPIO.LOW)
+	time.sleep(0.3)
